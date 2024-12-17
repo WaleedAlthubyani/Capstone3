4eb141fb3945b95cf8047a511f869dff8fe20829
Mshari-Alshashai <msharialshashai@gmail.com>
2024-12-17 10:35:52 +0300

    ?

--- src/main/java/com/example/capstone3/Model/Category.java
+++ src/main/java/com/example/capstone3/Model/Category.java
@@ -1,4 +1,32 @@
 package com.example.capstone3.Model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty(message = "Name is required")
+    @Column(nullable = false)
+    private String name;
+
+    @NotEmpty(message = "Description is required")
+    @Column(nullable = false)
+    private String description;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Artifact> artifacts;
 }
--- src/main/java/com/example/capstone3/Repository/TagRepository.java
+++ src/main/java/com/example/capstone3/Repository/TagRepository.java
@@ -1,4 +1,9 @@
 package com.example.capstone3.Repository;
 
-public interface TagRepository {
+import com.example.capstone3.Model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Integer> {
 }
